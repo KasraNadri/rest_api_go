e0bc6b8fdf025f8928f51d269519800091de127e
Kasra Nadri <kasra.n2018@gmail.com>
2025-02-25 09:27:14 +0330

    refactored code and tested getall, create and getbyid

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"rest_api/db"
-	"rest_api/models"
+	"rest_api/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,27 +11,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
-	server.POST("/events", createEvent)
+	routes.RegisterEvents(server)
 
 	server.Run(":8080") //localhost: 8080
-}
-
-func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data!"})
-
-	}
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
-	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 }
---
+++ routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterEvents(server *gin.Engine) {
+	server.GET("/events", getEvents)    // --- GET
+	server.GET("/events/:id", getEvent) // --- GET(BY ID)
+	server.POST("/events", createEvent) // --- POST
+}
