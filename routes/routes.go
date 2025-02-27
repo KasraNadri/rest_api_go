69964136a9d85d0fa75b84ac952d78992a787213
Kasra Nadri <kasra.n2018@gmail.com>
2025-02-27 12:51:59 +0330

    implemented the update route

--- routes/routes.go
+++ routes/routes.go
@@ -3,7 +3,8 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterEvents(server *gin.Engine) {
-	server.GET("/events", getEvents)    // --- GET
-	server.GET("/events/:id", getEvent) // --- GET(BY ID)
-	server.POST("/events", createEvent) // --- POST
+	server.GET("/events", getEvents)       // --- GET
+	server.GET("/events/:id", getEvent)    // --- GET(BY ID)
+	server.POST("/events", createEvent)    // --- POST
+	server.PUT("/events/:id", updateEvent) // --- PUT
 }
